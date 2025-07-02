005dbfd4574fd3460aba453b672eb07ff02b174c
Marc Espinosa i Tejada(W135175) <marc.espinosa@worldline.com>
2025-07-02 15:28:25 +0200

    On USER deleted, API keys related deleted on cascade.

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -3,6 +3,7 @@ import { Type } from 'class-transformer';
 import mongoose from "mongoose";
 import {UserRole} from "@modules/users/model/contract/user.interface";
 import {IsEnum} from "class-validator";
+import {ApiKeySchema} from "@common/auth/schemas/apikey.schema";
 
 export type UserDocument = User & mongoose.Document;
 
@@ -35,6 +36,16 @@ export class User {
 	updatedAt: Date;
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+const UserSchema = SchemaFactory.createForClass(User);
 
 UserSchema.index({ email: 1}, { unique: true });
+
+UserSchema.pre(['deleteOne', 'deleteMany', 'findOneAndDelete'], async function () {
+	if (this instanceof mongoose.Query) {
+		const document = await this.model.findOne(this.getQuery());
+		const ApiKeys = this.model.db.model('apikeys', ApiKeySchema);
+		await ApiKeys.deleteMany({ owner: document._id.toString() });
+	}
+});
+
+export {UserSchema};
